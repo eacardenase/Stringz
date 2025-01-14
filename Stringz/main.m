d8dd2a06924d21621cdba6bdcba2a637a77a177a
Edwin Cardenas <ecardenas@lwolf.com>
2025-01-14 17:31:26 -0500

    feat: write nsstring to the file system

--- Stringz/main.m
+++ Stringz/main.m
@@ -9,8 +9,18 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSMutableString *str = [[NSMutableString alloc] init];
+        
+        for (int i = 0; i < 10; i++) {
+            [str appendString:@"Edwin is cool!\n"];
+        }
+        
+        [str writeToFile:@"/tmp/cool.txt"
+              atomically:YES
+                encoding:NSUTF8StringEncoding
+                   error:NULL];
+        
+        NSLog(@"done writting /tmp/cool.txt");
     }
     return 0;
 }
