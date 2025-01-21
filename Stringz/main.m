78f7cb5d27dda460d183b7aab907a86d24e2147b
Edwin Cardenas <ecardenas@lwolf.com>
2025-01-20 21:08:39 -0500

    feat: read from the file system

--- Stringz/main.m
+++ Stringz/main.m
@@ -9,23 +9,38 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        NSMutableString *str = [[NSMutableString alloc] init];
+        // MARK: - Writing into the file system
+        
+        NSMutableString *writeStr = [[NSMutableString alloc] init];
         
         for (int i = 0; i < 10; i++) {
-            [str appendString:@"Edwin is cool!\n"];
+            [writeStr appendString:@"Edwin is cool!\n"];
         }
         
-        NSError *error;
+        NSError *writeError;
         
-        BOOL success = [str writeToFile:@"/too/darned/bad.txt"
-              atomically:YES
-                encoding:NSUTF8StringEncoding
-                   error:&error];
+        BOOL success = [writeStr writeToFile:@"/too/darned/bad.txt"
+                             atomically:YES
+                               encoding:NSUTF8StringEncoding
+                                  error:&writeError];
         
         if (success) {
             NSLog(@"done writting /tmp/cool.txt");
         } else {
-            NSLog(@"writing /tmp/cool.txt failed: %@", [error localizedDescription]);
+            NSLog(@"writing /tmp/cool.txt failed: %@", [writeError localizedDescription]);
+        }
+        
+        // MARK: - Reading from the file system
+        
+        NSError *readError;
+        NSString *readStr = [[NSString alloc] initWithContentsOfFile:@"/etc/resolv.conf"
+                                                        encoding:NSASCIIStringEncoding
+                                                           error:&readError];
+        
+        if (!readStr) {
+            NSLog(@"read failed: %@", [readError localizedDescription]);
+        } else {
+            NSLog(@"resolv.conf looks like this:\n%@", readStr);
         }
     }
     return 0;
